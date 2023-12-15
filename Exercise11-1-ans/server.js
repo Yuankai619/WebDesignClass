a7aa34fddfc8b5828c21c3e6886278e36e00369a
Yuankai619 <Yuankai_@outlook.com>
2023-12-16 04:02:39 +0800

    update homework11-1

--- Exercise11-1-ans/server.js
+++ Exercise11-1-ans/server.js
@@ -2,12 +2,13 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const Student = require('./student');
-
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.json());
-mongoose.connect('mongodb://localhost:27017/studentDB', { useNewUrlParser: true, useUnifiedTopology: true });
+password = "zHaGX0bMvEeVqUNf";
+mongoose.connect('mongodb+srv://Yuankai:zHaGX0bMvEeVqUNf@studendtdb.tdwgvin.mongodb.net/?retryWrites=true&w=majority');//, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -32,7 +33,31 @@ app.post('/students', async (req, res) => {
     }
 });
 
+app.delete('/students/:id', async (req, res) => {
+    try {
+        console.log(req.params.id);
+        const student = await Student.findByIdAndDelete(req.params.id);
+        if (!student) {
+            return res.status(404).send("No student found with that ID");
+        }
+        res.status(200).send("Student deleted successfully");
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+
+app.put('/students/:id', async (req, res) => {
+    try {
+        const student = await Student.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        if (!student) res.status(404).send("No student found");
+        res.json(student);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
+//zHaGX0bMvEeVqUNf
